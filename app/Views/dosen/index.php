0aa3e92ee770c6e7c284e0879160335bbda7c3f2
holiq <holiq.ibrahim376@gmail.com>
2024-03-17 17:26:22 +0700

    feat: add list dosen

---
+++ app/Views/dosen/index.php
@@ -0,0 +1,40 @@
+<?= $this->extend('layout') ?>
+
+<?= $this->section('content') ?>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <?php if(! empty(session()->getFlashdata('message'))) : ?>
+                <div class="alert alert-success">
+                    <?= session()->getFlashdata('message'); ?>
+                </div>
+                <?php endif ?>
+
+                <a href="#" class="btn btn-md btn-success mb-3">Tambah Data</a>
+                <table class="table table-bordered table-striped">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>Kode Dosen</th>
+                            <th>Nama Dosen</th>
+                            <th>Status Dosen</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($data as $dosen) : ?>
+                            <tr>
+                                <td><?= $dosen['kode_dosen'] ?></td>
+                                <td><?= $dosen['nama_dosen'] ?></td>
+                                <td><?= $dosen['status_dosen'] ?></td>
+                                <td class="text-center">
+                                 	  <a href="/dosen/update/<?= $dosen['id']; ?>">Edit</a>
+                                    <a href="/dosen/delete/<?= $dosen['id']; ?>">Delete</a>
+                                </td>
+                            </tr>
+                        <?php endforeach ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+<?= $this->endSection() ?>
