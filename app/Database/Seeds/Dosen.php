82abac70efef8dd2fabfacc5327dcd067e780e80
holiq <holiq.ibrahim376@gmail.com>
2024-03-30 21:10:02 +0700

    feat: add seeders

--- app/Database/Seeds/Dosen.php
+++ app/Database/Seeds/Dosen.php
@@ -2,11 +2,45 @@
 
 namespace App\Database\Seeds;
 
+use App\Models\Dosen as ModelsDosen;
 use CodeIgniter\Database\Seeder;
 
 class Dosen extends Seeder
 {
     public function run()
     {
+        $data = [
+            [
+                'kode_dosen'   => '001',
+                'nama_dosen'   => 'Agus S.Kom',
+                'status_dosen' => true,
+            ],
+            [
+                'kode_dosen'   => '002',
+                'nama_dosen'   => 'Wahyu M.Kom',
+                'status_dosen' => true,
+            ],
+            [
+                'kode_dosen'   => '003',
+                'nama_dosen'   => 'Zia S.H',
+                'status_dosen' => false,
+            ],
+            [
+                'kode_dosen'   => '004',
+                'nama_dosen'   => 'Cahyo M.M',
+                'status_dosen' => true,
+            ],
+            [
+                'kode_dosen'   => '005',
+                'nama_dosen'   => 'Bayu S.Pd.I',
+                'status_dosen' => false,
+            ],
+        ];
+
+        $dosen = new ModelsDosen();
+
+        foreach ($data as $row) {
+            $dosen->insert($row);
+        }
     }
 }
--- app/Database/Seeds/User.php
+++ app/Database/Seeds/User.php
@@ -2,11 +2,35 @@
 
 namespace App\Database\Seeds;
 
+use App\Models\User as ModelsUser;
 use CodeIgniter\Database\Seeder;
 
 class User extends Seeder
 {
     public function run()
     {
+        $data = [
+            [
+                'name'     => 'Holiq',
+                'username' => 'holiq',
+                'password' => password_hash('11111111', PASSWORD_DEFAULT),
+            ],
+            [
+                'name'     => 'Johan',
+                'username' => 'johan',
+                'password' => password_hash('11111111', PASSWORD_DEFAULT),
+            ],
+            [
+                'name'     => 'Agus',
+                'username' => 'agus',
+                'password' => password_hash('11111111', PASSWORD_DEFAULT),
+            ],
+        ];
+
+        $user = new ModelsUser();
+
+        foreach ($data as $row) {
+            $user->insert($row);
+        }
     }
 }
