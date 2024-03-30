a52de7bec7ca8f69e7c94a0e42d442acca65641d
holiq <holiq.ibrahim376@gmail.com>
2024-03-30 20:45:53 +0700

    fix(migration): invalid format migration

--- app/Database/Migrations/2024-03-17-062524_Dosen.php
+++ app/Database/Migrations/2024-03-17-062524_Dosen.php
@@ -8,33 +8,26 @@ class Dosen extends Migration
 {
     public function up()
     {
-        $this->forge->addField(
-            [
-                'id_dosen' => [
-                    'type'         => 'INT',
-                    'unsigned'     => true,
-                    'autoincrment' => 'true',
-                ],
+        $this->forge->addField([
+            'id_dosen' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
             ],
-            [
-                'kode_dosen' => [
-                    'type'      => 'VARCHAR',
-                    'constrait' => 20,
-                ],
+            'kode_dosen' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 20,
             ],
-            [
-                'nama_dosen' => [
-                    'type'      => 'VARCHAR',
-                    'constrait' => 30,
-                ],
+            'nama_dosen' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 30,
             ],
-            [
-                'status_dosen' => [
-                    'type'      => 'CHAR',
-                    'constrait' => 1,
-                ],
-            ]
-        );
+            'status_dosen' => [
+                'type'       => 'CHAR',
+                'constraint' => 1,
+            ],
+        ]);
         $this->forge->addKey('id_dosen', true);
         $this->forge->createTable('dosen');
     }
