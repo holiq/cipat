83eab6ba217de74c1502945c97d48dfa76559d69
holiq <holiq.ibrahim376@gmail.com>
2024-03-30 20:51:11 +0700

    feat: setup table users

---
+++ app/Database/Migrations/2024-03-30-133455_User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class User extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 30,
+            ],
+            'username' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 10,
+            ],
+            'password' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
